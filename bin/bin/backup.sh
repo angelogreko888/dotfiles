3d506fa62b763f2b4f00543ee31f7b3c3c8d9796
angelogreko <angelogreko@gmail.com>
2024-11-17 08:51:27 +0200

    save

--- bin/bin/backup.sh
+++ bin/bin/backup.sh
@@ -2,6 +2,16 @@ sleep 1
 
 rm -rf ~/.cache/* 
 
+sleep 1
+
+pacman -Qqe | grep -v "$(pacman -Qqm)" > $HOME/vago/pc/inst/pacman.lst
+
+sleep 1
+
+pacman -Qqm > $HOME/vago/pc/inst/aur.lst
+
+sleep 1
+
 ~/bin/bup &
 
 sleep 1

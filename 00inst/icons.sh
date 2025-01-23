5c21c518c257acd93dc3377cc83fdca73e29323e
angelogreko <angelogreko@gmail.com>
2025-01-23 18:08:28 +0200

    save

---
+++ 00inst/icons.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ -d /usr/share/icons/buuf-nestort/ ];then
+	echo "buuf exists"
+else
+	sudo cp -r  ~/dotfiles/nouse/icons/buuf-nestort  /usr/share/icons
+	echo "buuf copied!!!"
+fi
+
+if [ -d /usr/share/icons/radioactive-yellow/ ];then
+	echo "yellow exists"
+else
+	sudo cp -r  ~/dotfiles/nouse/icons/radioactive-yellow /usr/share/icons 
+	echo "yellow copied!!!"
+fi
+

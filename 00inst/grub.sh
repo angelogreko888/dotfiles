def9aa10c9e4c6bb27a673c4f24922223be97529
angelogreko <angelogreko@gmail.com>
2025-01-23 17:43:19 +0200

    save

--- 00inst/grub.sh
+++ 00inst/grub.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
-sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/' /etc/default/grub
+handle_error() {
+      echo "An error occurred on line $1"
+      exit 1
+  }
 
-echo "Done!"
+  trap 'handle_error $LINENO' ERR
 
-sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/' /etc/default/grub
+
+
+if sudo grep -q "GRUB_TIMEOUT=1" /etc/default/grub; then
+	sudo grub-mkconfig -o /boot/grub/grub.cfg
+    fi
+
+

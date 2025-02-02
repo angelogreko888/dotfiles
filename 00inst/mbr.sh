828d9c4962a97d08f2ebc234fcddf3a9703b3478
angelogreko <angelogreko@gmail.com>
2025-02-02 12:02:44 +0200

    save

--- 00inst/mbr.sh
+++ 00inst/mbr.sh
@@ -13,14 +13,15 @@ echo root:password | chpasswd
 
 pacman -S --needed --noconfirm base-devel efibootmgr grub linux-headers networkmanager pipewire pipewire-alsa pipewire-jack pipewire-pulse wpa_supplicant
 
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-install --target=i386-pc /dev/XXX
 grub-mkconfig -o /boot/grub/grub.cfg
 
 useradd -m vago
 echo vago:password | chpasswd
 
 echo "vago ALL=(ALL) ALL" >> /etc/sudoers.d/vago
-echo "Defaults  timestamp_timeout=999" >> /etc/sudoers.d/vago
+echo "Defaults	timestamp_timeout=999" >> /etc/sudoers.d/vago
 
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+
 

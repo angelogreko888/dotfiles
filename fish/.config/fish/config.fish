55f600f0bb6b6fa6f9636bd84618f3159b1e2588
angelogreko <angelogreko@gmail.com>
2025-02-05 14:47:42 +0200

    save

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,14 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-#function fish_prompt
-
-  # interactive user name @ host name, date/time in YYYY-mm-dd format and path
-
-#  echo  ∋∈ ∋∈ ∋∈   (pwd)   
-
-#  echo "\$ "
-
-#end
-
+set -g fish_greeting
 

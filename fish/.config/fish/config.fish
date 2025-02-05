19d1e414d46afb60ab37d44997c5512d44433c19
angelogreko <angelogreko@gmail.com>
2025-02-05 15:36:16 +0200

    save

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,3 +4,18 @@ end
 
 set -g fish_greeting
 
+set -l last_status $status
+
+if test $last_status -ne 0
+    set stat (set_color red)"[$last_status]"(set_color normal)
+end
+
+function fish_prompt
+    set -l last_status $status
+    set -l stat
+    if test $last_status -ne 0
+        set stat (set_color red)"[$last_status]"(set_color normal)
+    end
+
+    echo \n (set_color yellow)∋∈  ∋∈  ∋∈    (set_color green) (prompt_pwd)  (set_color red) $stat ' '
+end

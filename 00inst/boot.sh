5c74b6f151f773fec6d09bced628bb35215a6673
angelogreko <angelogreko@gmail.com>
2025-02-15 01:26:56 +0200

    save

--- 00inst/boot.sh
+++ 00inst/boot.sh
@@ -13,7 +13,21 @@ echo root:password | chpasswd
 
 pacman -S --needed --noconfirm nano base-devel linux-headers networkmanager pipewire pipewire-alsa pipewire-jack pipewire-pulse wpa_supplicant
 
+bootctl --path=/boot install
 
+echo "
+default  arch.conf
+timeout  1
+console-mode max
+editor   no
+" > /boot/loader/loader.conf
+
+echo "
+title   Arch Linux
+linux   /vmlinuz-linux
+initrd  /initramfs-linux.img
+options root=UUID=  rw quite splash
+" > /boot/loader/entries/arch.conf
 
 useradd -m vago
 echo vago:password | chpasswd
@@ -22,3 +36,4 @@ echo "vago ALL=(ALL) ALL" >> /etc/sudoers.d/vago
 echo "Defaults  timestamp_timeout=999" >> /etc/sudoers.d/vago
 
 systemctl enable NetworkManager
+systemctl enable systemd-boot-update.service

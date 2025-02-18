1689920d3ff1ed88574cd1a4ef2938bf9a439770
angelogreko <angelogreko@gmail.com>
2025-02-18 16:26:41 +0200

    save

--- 00inst/011efi.sh
+++ 00inst/011efi.sh
@@ -11,7 +11,7 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 greco.localdomain greco" >> /etc/hosts
 echo root:password | chpasswd
 
-pacman -S --needed --noconfirm nano base-devel efibootmgr grub linux-headers networkmanager wpa_supplicant
+pacman -S --needed --noconfirm sudo nano base-devel efibootmgr grub linux-headers networkmanager wpa_supplicant
 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg

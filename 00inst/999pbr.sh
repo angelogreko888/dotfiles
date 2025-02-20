da78e06db03158ef0d832ec72fdc2d63d0f3788d
angelogreko <angelogreko@gmail.com>
2025-02-20 14:17:08 +0200

    save

--- 00inst/999pbr.sh
+++ 00inst/999pbr.sh
@@ -8,15 +8,21 @@ handle_error() {
   trap 'handle_error $LINENO' ERR
 
 
-cd ~
+cd ~/Downloads
 
-sudo pacman -Su dpkg
+#sudo pacman -Su dpkg
 
-yay -Su --noconfirm -- needed brother-mfc-1910w
+#yay -Su --noconfirm -- needed brother-mfc-1910w
+
+curl -O https://download.brother.com/welcome/dlf006893/linux-brprinter-installer-2.2.4-1.gz
+
+gunzip linux-brprinter-installer-2.2.4-1.gz 
+
+sudo bash linux-brprinter-installer-2.2.4-1
 
 curl -O https://download.brother.com/welcome/dlf105191/brmfcfaxdrv-2.0.2-1.amd64.deb
 
-sudo dpkg -i --force-all brmfcfaxdrv-2.0.2-1.amd64.deb
+#sudo dpkg -i --force-all brmfcfaxdrv-2.0.2-1.amd64.deb
 
-rm brmfcfaxdrv-2.0.2-1.amd64.deb
+#rm *
 

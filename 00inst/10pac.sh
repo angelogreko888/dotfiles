e3b2ce2c25d2bfbd859c73319463dac36ad53bc9
angelogreko <angelogreko@gmail.com>
2025-02-23 00:26:56 +0200

    save

--- 00inst/10pac.sh
+++ 00inst/10pac.sh
@@ -11,7 +11,6 @@ source ~/dotfiles/00inst/pac.lst
 
 cd ~
 
-#cat ~/dotfiles/00inst/pac.lst | xargs sudo pacman -S --needed --noconfirm
 
 for PKG in "${lst[@]}";do
         sudo pacman -Su --needed --noconfirm "$PKG"

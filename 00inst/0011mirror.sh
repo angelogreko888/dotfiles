e46667360e2163147767a8ccb3b321054ad4b043
angelogreko <angelogreko@gmail.com>
2025-02-24 15:00:36 +0200

    save

--- 00inst/0011mirror.sh
+++ 00inst/0011mirror.sh
@@ -8,12 +8,12 @@ handle_error() {
   trap 'handle_error $LINENO' ERR
 
 case $1 in
-        i)  sudo  reflector --age 1 --sort rate --country Israel --protocol https --verbose --save /etc/pacman.d/mirrorlist ;
+        i) sudo  reflector --age 1 --sort rate --country Israel --protocol https --verbose --save /etc/pacman.d/mirrorlist ;
 		sudo sed -rie '/mivzakim/d' /etc/pacman.d/mirrorlist  ;
 		echo "
 Server = https://mirror.isoc.org.il/pub/archlinux/$repo/os/$arch
 		" | sudo tee -a /etc/pacman.d/mirrorlist ;;
-        c)  sudo  reflector --age 1 --sort rate -n 5 --country Germany,Greece,Turkey,Israel --protocol https --verbose --save /etc/pacman.d/mirrorlist ;;
+        c) sudo  reflector --age 1 --sort rate -n 5 --country Germany,Greece,Turkey,Israel,United Kingdom --protocol https --verbose --save /etc/pacman.d/mirrorlist ;;
         *) echo "enter country" && exit 0 ;;
 esac
 

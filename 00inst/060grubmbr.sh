0ed6323d3d3da0ca9621c3921d6e6506a6d9b445
angelogreko <angelogreko@gmail.com>
2025-03-04 23:00:44 +0200

    save

--- 00inst/060grubmbr.sh
+++ 00inst/060grubmbr.sh
@@ -1,18 +1,37 @@
 #!/usr/bin/env bash
 
-ln -sf /usr/share/zoneinfo/Europe/Nicosia /etc/localtime
+printf "\e[1;32m.......Enter Country.......: \n\n\e[0m"
+select  cn in Israel Cyprus;do
+	case $cn in
+        Israel) 
+		ln -sf /usr/share/zoneinfo/Israel /etc/localtime
+		break ;;
+        Cyprus) 
+		ln -sf /usr/share/zoneinfo/Europe/Nicosia /etc/localtime
+		break ;;
+	*) 
+		printf "\n\e[1;31m... !!! ERROR Enter Your Choice !!! ... \e[0m" >&2
+	esac
+done
+
 hwclock --systohc
 sed -i '171s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "greco" >> /etc/hostname
-echo "127.0.0.1 localhost" >> /etc/hosts
-echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 greco.localdomain greco" >> /etc/hosts
+#echo "127.0.0.1 localhost" >> /etc/hosts
+#echo "::1       localhost" >> /etc/hosts
+#echo "127.0.1.1 greco.localdomain greco" >> /etc/hosts
 
-printf "\n\e[1;32mEnter root password: \e[0m"
-read -s rp
-echo root:$rp | chpasswd
+printf "\n\e[1;32mEnter Root password \n\e[0m"
+while true; do
+  read -s -p "Password: " rp
+  echo
+  read -s -p "Password (again): " rpr
+  echo
+  [ "$rp" = "$rpr" ] &&  echo root:$rp | chpasswd && break
+  echo "Please try again"
+done
 
 pacman -S --needed --noconfirm sudo wget git nano base-devel grub linux-headers networkmanager wpa_supplicant
 
@@ -26,9 +45,15 @@ grub-mkconfig -o /boot/grub/grub.cfg
 
 useradd -m vago
 
-printf "\n\e[1;32mEnter user password: \e[0m"
-read -s up
-echo vago:$up | chpasswd
+printf "\n\e[1;32mEnter User password \n\e[0m"
+while true; do
+  read -s -p "Password: " up
+  echo
+  read -s -p "Password (again): " upr
+  echo
+  [ "$up" = "$upr" ] &&  echo root:$up | chpasswd && break
+  echo "Please try again"
+done
 
 echo "vago ALL=(ALL) ALL" >> /etc/sudoers.d/vago
 echo "Defaults	timestamp_timeout=999" >> /etc/sudoers.d/vago

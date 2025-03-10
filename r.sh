1ce234e94faf1bb2b99cdb2bc7b323067cbc6e25
angelogreko <angelogreko@gmail.com>
2025-03-10 11:07:22 +0200

    save

--- r.sh
+++ r.sh
@@ -1,50 +1,41 @@
 #!/usr/bin/env bash
 
 gh="https://raw.githubusercontent.com/angelogreko888/dotfiles/main/00inst/"
-
 cp="010confpac.sh"
 bt="030boot.sh"
 ef="050grubefi.sh"
-mb="060grubmbr.sh"
-
-lst=(
-$cp
-$bt
-$ef
-$mb
-)
+gt="060grubgpt.sh"
 
 pacman -Sy --needed --noconfirm  wget nano reflector
 
-for SU in "${lst[@]}";do
-	wget $gh$SU
-done
-
-if ! [ -f "$cp" ] && [ -f "$bt" ]  && [ -f "$ef" ] && [ -f "$mb" ];then
-	printf "\n\e[1;31m!!! Something went wrong- missing file !!! \e[0m"
-        exit 1 
-fi
-
-chmod +x 0*
-
-bash $cp
-
 printf "\n\e[1;32m... Enter your choice ... : \n\e[0m"
 
-select  bm in boot efi gpt;do
+select  bm in efi boot gpt;do
 	case $bm in
-		boot)
-			bash $bt 
-			break ;;
 		efi)
-			bash $ef 
+                        wget $gh$ef $gh$cp
+                        bb=$ef
+			break ;;
+		boot)
+                        wget $gh$bt $gh$cp
+                        bb=$bt 
 			break ;;
 		gpt)
-			bash $mb 
+                        wget $gh$gt $gh$cp
+                        bb=$gt
 			break ;;
 		*)
 		printf "\n\e[1;31m... !!! ERROR Enter Your Choice !!! ... \e[0m" >&2
 	esac
 done
+
+if  [ -f "$cp" ] && [ -f "$bb" ];then
+        chmod +x 0*
+        bash $cp
+        bash $bb 
+else
+        printf "\n\e[1;31m!!! Something went wrong- missing file !!! \e[0m"
+        exit 1 
+fi
 
 printf "\n\n\e[1;32m ... Done Root Setup! Type exit, umount -a and reboot ...\n\n\e[0m"

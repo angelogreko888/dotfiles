80bc64fad1b0ca32c8e6ec08fd4b8ea5a7355a44
angelogreko <angelogreko@gmail.com>
2025-03-10 10:03:42 +0200

    save

--- i.sh
+++ i.sh
@@ -1,95 +1,68 @@
 #!/usr/bin/env bash
 
+ld=(
+boot
+swap
+home
+vt
+)
+
 lsblk
 
+printf "\n\e[1;32m.......hard disk type? : \n\e[0m"
+select  ns in nvme sda;do
+        case $ns in
+        nvme)
+        aa=nvme0n1p
+        break ;;
+        sda)
+        aa=sda
+        break ;;
+        *)
+         printf "\n\e[1;31m... !!! ERROR Enter Your Choice !!! ... \e[0m" >>
+        esac
+done
 
-printf "\n\e[1;32mEnter Root directory name: \e[0m"
+printf "\n\e[1;32mEnter Root directory number : \e[0m"
 read rt
-mkfs.ext4 /dev/nvme0n1p$rt 
-mount /dev/nvme0n1p$rt /mnt
+mkfs.ext4 /dev/$aa$rt 
+mount /dev/$aa$rt /mnt
 
-printf "\e[1;32m.......boot?: \n\e[0m"
-select  yn in yes no;do
-        case $yn in
-        yes) 
-		printf "\n\e[1;32mEnter Boot directory name: \e[0m"
-		read bd
-                printf "\n\e[1;32m... confirm boot : "$bd" \e[0m"
-                read
-		mkfs.fat -F32 /dev/nvme0n1p$bd
-		mount --mkdir /dev/nvme0n1p$bd /mnt/boot 
-                break ;;
-        no) 
-		printf "\n\e[1;32mNo Boot directory \n\e[0m"
-                break ;;
-        *) 
-                printf "\n\e[1;31m... !!! ERROR Enter Your Choice !!! ... \e[0m" >&2
-        esac
+for LD in "${ld[@]}";do
+	printf "\n\e[1;32m.......mount "$LD" ?: \n\e[0m"
+	select  yn in yes no;do
+        	case $yn in
+        	yes) 
+			printf "\n\e[1;32mEnter"$LD" directory name: \e[0m"
+			read bd
+			case $LD in
+				boot)
+					mkfs.fat -F32 /dev/$aa$bd
+					mount /dev/$aa$bd /mnt/$LD
+					break ;;
+				swap)
+					mkswap /dev/$aa$bd
+					swapon /dev/$aa$bd 
+                			break ;;
+				home)
+					mkfs.ext4 /dev/$aa$bd
+					mount --mkdir /dev/$aa$bd /mnt/$LD
+					break ;;
+				vt)
+                                        mkfs.ext4 /dev/$aa$bd
+                                        mount --mkdir /dev/$aa$bd /mnt/$LD
+                                        break ;;
+			esac
+		break ;;
+
+        	no) 
+			printf "\n\e[1;32mNo "$LD" directory \n\e[0m"
+        	        break ;;
+       		 *) 
+                	printf "\n\e[1;31m... !!! ERROR Enter Your Choice !!! ... \e[0m" >&2
+        	esac
+	done
 done
-
-
-printf "\e[1;32m.......swap?: \n\e[0m"
-select  yn in yes no;do
-        case $yn in
-        yes) 
-                printf "\n\e[1;32mEnter swap directory name: \e[0m"
-                read sw
-		printf "\n\e[1;32m... confirm swap : "$sw" \e[0m"
-		read
-		mkswap /dev/nvme0n1p$sw 
-		swapon /dev/nvme0n1p$sw 
-                break ;;
-        no) 
-                printf "\n\e[1;32mNo swap directory \n\e[0m"
-                break ;;
-        *) 
-                printf "\n\e[1;31m... !!! ERROR Enter Your Choice !!! ... \e[0m" >&2
-        esac
-done
-
-
-printf "\e[1;32m.......home?: \n\e[0m"
-select  yn in yes no;do
-        case $yn in
-        yes) 
-		printf "\n\e[1;32mEnter Home directory name: \e[0m"
-		read hm
-                printf "\n\e[1;32m... confirm home : "$hm" \e[0m"
-                read
-		mkfs.ext4 /dev/nvme0n1p$hm
-		mount --mkdir /dev/nvme0n1p$hm /mnt/home
-                break ;;
-        no) 
-                printf "\n\e[1;32mNo Home directory \n\e[0m"
-                break ;;
-        *) 
-                printf "\n\e[1;31m... !!! ERROR Enter Your Choice !!! ... \e[0m" >&2
-        esac
-done
-
-printf "\e[1;32m.......vt?: \n\e[0m"
-select  yn in yes no;do
-        case $yn in
-        yes) 
-                printf "\n\e[1;32mEnter vt directory name: \e[0m"
-                read vt
-                printf "\n\e[1;32m... confirm vt : "$vt" \e[0m"
-                read
-		mkfs.ext4 /dev/nvme0n1p$vt
-                mount --mkdir /dev/nvme0n1p$vt /mnt/vt
-                break ;;
-        no) 
-                printf "\n\e[1;32mNo vt directory \n\e[0m"
-                break ;;
-        *) 
-                printf "\n\e[1;31m... !!! ERROR Enter Your Choice !!! ... \e[0m" >&2
-        esac
-done
-
-lsblk
-
-printf "\e[1;32m.......enter to continue?: \n\e[0m"
-read 
 
 pacstrap -K /mnt base base-devel linux linux-firmware
 
@@ -98,4 +71,3 @@ genfstab -U /mnt >> /mnt/etc/fstab
 nano /mnt/etc/fstab
 
 arch-chroot /mnt
-
---
+++ nouse/00inst/i.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+
+lsblk
+
+
+printf "\n\e[1;32mEnter Root directory name: \e[0m"
+read rt
+mkfs.ext4 /dev/nvme0n1p$rt 
+mount /dev/nvme0n1p$rt /mnt
+
+printf "\e[1;32m.......boot?: \n\e[0m"
+select  yn in yes no;do
+        case $yn in
+        yes) 
+		printf "\n\e[1;32mEnter Boot directory name: \e[0m"
+		read bd
+                printf "\n\e[1;32m... confirm boot : "$bd" \e[0m"
+                read
+		mkfs.fat -F32 /dev/nvme0n1p$bd
+		mount --mkdir /dev/nvme0n1p$bd /mnt/boot 
+                break ;;
+        no) 
+		printf "\n\e[1;32mNo Boot directory \n\e[0m"
+                break ;;
+        *) 
+                printf "\n\e[1;31m... !!! ERROR Enter Your Choice !!! ... \e[0m" >&2
+        esac
+done
+
+
+printf "\e[1;32m.......swap?: \n\e[0m"
+select  yn in yes no;do
+        case $yn in
+        yes) 
+                printf "\n\e[1;32mEnter swap directory name: \e[0m"
+                read sw
+		printf "\n\e[1;32m... confirm swap : "$sw" \e[0m"
+		read
+		mkswap /dev/nvme0n1p$sw 
+		swapon /dev/nvme0n1p$sw 
+                break ;;
+        no) 
+                printf "\n\e[1;32mNo swap directory \n\e[0m"
+                break ;;
+        *) 
+                printf "\n\e[1;31m... !!! ERROR Enter Your Choice !!! ... \e[0m" >&2
+        esac
+done
+
+
+printf "\e[1;32m.......home?: \n\e[0m"
+select  yn in yes no;do
+        case $yn in
+        yes) 
+		printf "\n\e[1;32mEnter Home directory name: \e[0m"
+		read hm
+                printf "\n\e[1;32m... confirm home : "$hm" \e[0m"
+                read
+		mkfs.ext4 /dev/nvme0n1p$hm
+		mount --mkdir /dev/nvme0n1p$hm /mnt/home
+                break ;;
+        no) 
+                printf "\n\e[1;32mNo Home directory \n\e[0m"
+                break ;;
+        *) 
+                printf "\n\e[1;31m... !!! ERROR Enter Your Choice !!! ... \e[0m" >&2
+        esac
+done
+
+printf "\e[1;32m.......vt?: \n\e[0m"
+select  yn in yes no;do
+        case $yn in
+        yes) 
+                printf "\n\e[1;32mEnter vt directory name: \e[0m"
+                read vt
+                printf "\n\e[1;32m... confirm vt : "$vt" \e[0m"
+                read
+		mkfs.ext4 /dev/nvme0n1p$vt
+                mount --mkdir /dev/nvme0n1p$vt /mnt/vt
+                break ;;
+        no) 
+                printf "\n\e[1;32mNo vt directory \n\e[0m"
+                break ;;
+        *) 
+                printf "\n\e[1;31m... !!! ERROR Enter Your Choice !!! ... \e[0m" >&2
+        esac
+done
+
+lsblk
+
+printf "\e[1;32m.......enter to continue?: \n\e[0m"
+read 
+
+pacstrap -K /mnt base base-devel linux linux-firmware
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+nano /mnt/etc/fstab
+
+arch-chroot /mnt
+

68f73dda6f8a43854ad8b0baa5ec38bc3fe08253
angelogreko <angelogreko@gmail.com>
2025-03-15 09:10:10 +0200

    save

--- bin/bin/tshift.sh
+++ bin/bin/tshift.sh
@@ -8,6 +8,6 @@ handle_error() {
   trap 'handle_error $LINENO' ERR
 
 
-sudo timeshift --create;
-a=$(sudo timeshift --list |awk 'NR==12{print $3}');
-sudo timeshift  --delete  --snapshot $a;
+sudo timeshift --create
+a=$(sudo timeshift --list | awk 'NR==12{print $3}')
+sudo timeshift --delete --snapshot $a

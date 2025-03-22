42b36a04abfb3fe6069c77caefecee8285633225
angelogreko <angelogreko@gmail.com>
2025-03-22 21:01:33 +0200

    save

--- bin/bin/gpush.sh
+++ bin/bin/gpush.sh
@@ -10,6 +10,9 @@
 	rsync search-history.db  ~/dotfiles/freetube/.config/FreeTube/search-history.db
 	rsync settings.db  ~/dotfiles/freetube/.config/FreeTube/settings.db
 
+	cd ~
+	rsync -r --exclude 'lock' .mullvad/mullvadbrowser  ~/dotfiles/home/.mullvad/
+
 	cd ~/dotfiles
 	git add * && git commit -m "save" && git push -u origin main
 

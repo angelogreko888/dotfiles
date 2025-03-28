bd9ecdb239259c328ff8cf50d14b6e519e01cca8
angelogreko <angelogreko@gmail.com>
2025-03-28 20:00:35 +0300

    save

---
+++ nouse/bin/sync.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+
+for i in $(seq 1 99);
+do
+  sleep 900
+
+  wget -q --spider http://google.com
+
+  if [ $? -eq 0 ]; then
+
+  cd  ~/.config/FreeTube/
+  rsync history.db  ~/dotfiles/freetube/.config/FreeTube/history.db
+  rsync playlists.db  ~/dotfiles/freetube/.config/FreeTube/playlists.db
+  rsync profiles.db  ~/dotfiles/freetube/.config/FreeTube/profiles.db
+  rsync search-history.db  ~/dotfiles/freetube/.config/FreeTube/search-history.db
+  rsync settings.db  ~/dotfiles/freetube/.config/FreeTube/settings.db
+
+  cd ~
+  rsync -r --exclude 'lock' .mullvad  ~/dotfiles/nouse/brows/
+  rsync -r --exclude 'lock' .librewolf  ~/dotfiles/nouse/brows/
+
+
+  cd ~/dotfiles
+  git add * && git commit -m "save" && git push -u origin main
+
+  else
+  echo exit 
+  fi
+
+done

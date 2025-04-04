9fe936617aaff28d759e1fdf9da8e4a6369b7988
angelogreko <angelogreko@gmail.com>
2025-04-04 18:04:39 +0300

    save

--- bin/bin/paccach.sh
+++ bin/bin/paccach.sh
@@ -10,8 +10,17 @@ handle_error() {
 a=$(date | awk '{print $1}')
 
 if [ $a == Fri ];then
-	exec foot sh -c 'echo "paccash....";
-	sudo paccache -ruk0;
-	sudo paccache -rk1;
-	sleep 3'
+	printf "\n\e[1;32m.......clean paccache? : \n\e[0m"
+	select yn in yes no;do
+	case #yn in
+	yes)
+		exec foot sh -c 'sudo paccache -ruk0;
+		sudo paccache -rk1;
+		sleep 5'
+		break ;;
+	no)
+		exit 0
+		break ;;
+	esac
+done
 fi

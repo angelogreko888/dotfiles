d5e3d52b0d2f8fc7eb13223d97e2b018a2d1d2ea
angelogreko <angelogreko@gmail.com>
2025-04-14 02:51:29 +0300

    save

--- bin/bin/gitpull.sh
+++ bin/bin/gitpull.sh
@@ -8,14 +8,18 @@ handle_error() {
   trap 'handle_error $LINENO' ERR
 
 
+until ping -c1 www.google.com &>/dev/null
+do
+sleep 2
+done
 
 cd ~/dotfiles 
 
- git pull 
- 
- cd ~/dotfiles/freetube/.config/FreeTube/ 
-  rsync history.db  ~/.config/FreeTube/history.db 
-  rsync playlists.db  ~/.config/FreeTube/playlists.db 
-  rsync profiles.db  ~/.config/FreeTube/profiles.db 
-  rsync search-history.db  ~/.config/FreeTube/search-history.db 
-  rsync settings.db  ~/.config/FreeTube/settings.db 
+  exec bash foot sh -c  'git pull;
+  cd ~/dotfiles/freetube/.config/FreeTube/;
+  rsync history.db  ~/.config/FreeTube/history.db;
+  rsync playlists.db  ~/.config/FreeTube/playlists.db;
+  rsync profiles.db  ~/.config/FreeTube/profiles.db;
+  rsync search-history.db  ~/.config/FreeTube/search-history.db;
+  rsync settings.db  ~/.config/FreeTube/settings.db;
+  echo Done!!!; cd ~;  read'

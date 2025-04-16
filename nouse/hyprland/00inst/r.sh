03cf819b175b2504baa6ff0d619c246f67454356
angelogreko <angelogreko@gmail.com>
2025-04-16 10:19:26 +0300

    save

---
+++ nouse/hyprland/00inst/r.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+gh="https://raw.githubusercontent.com/angelogreko888/dotfiles/main/00inst/"
+ef="050grubefi.sh"
+gt="060grubgpt.sh"
+
+pacman -Sy --needed --noconfirm  wget nano 
+
+printf "\n\e[1;32m... Enter your choice ... : \n\e[0m"
+
+select  bm in efi gpt;do
+	case $bm in
+		efi)
+                        wget $gh$ef 
+                        bb=$ef
+			break ;;
+		gpt)
+                        wget $gh$gt 
+                        bb=$gt
+			break ;;
+		*)
+		printf "\n\e[1;31m... !!! ERROR Enter Your Choice !!! ... \e[0m" >&2
+	esac
+done
+
+if [ -f "$bb" ];then
+        chmod +x $bb
+        bash $bb 
+else
+        printf "\n\e[1;31m!!! Something went wrong- missing file !!! \e[0m"
+        exit 1 
+fi
+
+rm 0*
+
+printf "\n\n\e[1;32m ... Done Root Setup! Type exit, umount -a and reboot ...\n\n\e[0m"

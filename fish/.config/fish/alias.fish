6e3f814d19f7b7123f77092d309785d376540c2f
angelogreko <angelogreko@gmail.com>
2025-04-18 17:35:31 +0300

    save

--- fish/.config/fish/alias.fish
+++ fish/.config/fish/alias.fish
@@ -147,5 +147,7 @@ alias af='nano ~/.config/fish/alias.fish'
 alias mf=' sudo -sE mintstick -m format'
 alias mi=' sudo -sE mintstick -m iso'
 
+alias tlp='sudo -sE tlpui'
+
 alias fc="sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches'"
 alias mm='hyprctl keyword monitor HDMI-A-1,1920x1080@60.0,0x0,1'

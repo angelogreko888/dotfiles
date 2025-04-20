916515c202ed55bf7587dd493d6e587d5d9319ba
angelogreko <angelogreko@gmail.com>
2025-04-20 16:57:16 +0300

    save

--- bin/bin/trim.sh
+++ bin/bin/trim.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-handle_error() {
-      echo "An error occurred on line $1"
-      exit 1
-  }
-
-  trap 'handle_error $LINENO' ERR
 
 
 a=$(date | awk '{print $1}')
@@ -14,9 +8,11 @@ if [ $a == Fri ];then
                 exec foot sh -c '
 		 printf "\n\e[1;32m....... trim....... \n\e[0m"  
                 sudo fstrim -av;
-                echo date > ~/.log/trim.log;
-                read;'
+                echo $(date) > ~/.log/trim.log;
+                '
         fi
 else
-        rm ~/.log/trim.log 2>1 /dev/null
+	if [ -f ~/.log/trim.log ]; then
+		rm ~/.log/trim.log 
+	fi
 fi

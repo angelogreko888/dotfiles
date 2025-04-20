cec6e37fcde0903aea1bae30c6c92456a0f86bec
angelogreko <angelogreko@gmail.com>
2025-04-20 13:36:36 +0300

    save

--- bin/bin/dpaccach.sh
+++ bin/bin/dpaccach.sh
@@ -6,9 +6,14 @@ handle_error() {
 
   trap 'handle_error $LINENO' ERR
 
-		sudo paccache -ruk0;
-                sudo paccache -rk1;
-                sudo pacman -Scc --noconfirm
-                yay -Scc --noconfirm
-                yay -Yc
-                sudo pacman -Rnsc $(pacman -Qdtq)
+sudo paccache -ruk0;
+sudo paccache -rk1;
+sudo pacman -Scc --noconfirm
+yay -Scc --noconfirm
+yay -Yc
+a="$((pacman -Qdtq) | wc -l >/dev/null)"
+if [ $a > 0 ];then
+        sudo pacman -Rnsc $(pacman -Qdtq)
+else 
+        exit 0
+fi

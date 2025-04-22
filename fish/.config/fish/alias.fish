ef2e3873293a089b9cc31e01319d03333fa3d3e3
angelogreko <angelogreko@gmail.com>
2025-04-22 11:10:59 +0300

    save

--- fish/.config/fish/alias.fish
+++ fish/.config/fish/alias.fish
@@ -151,3 +151,5 @@ alias lp='sudo -sE tlpui'
 
 alias fc="sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches'"
 alias mm='hyprctl keyword monitor HDMI-A-1,1920x1080@60.0,0x0,1'
+alias dp='~/bin/dpaccach.sh'
+

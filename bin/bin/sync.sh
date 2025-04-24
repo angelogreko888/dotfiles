f32c89e575f2dadc1c5e2c40a2cb8ac4cfc884a8
angelogreko <angelogreko@gmail.com>
2025-04-25 01:02:22 +0300

    save

--- bin/bin/sync.sh
+++ bin/bin/sync.sh
@@ -9,31 +9,31 @@ handle_error() {
 flag="/var/tmp/$(basename -- $0).flag"
 
 if [ -e "$flag" ] ;then
-  [ $(date +%F) = $(date +%F -r "$flag") ] && exit 1
+	[ $(date +%F) = $(date +%F -r "$flag") ] && exit 1
 fi
 touch "$flag"
 
 for i in $(seq 1 999); do
-sleep 6
-cd  ~/.config/FreeTube/
-rsync history.db  ~/dotfiles/freetube/.config/FreeTube/history.db
-rsync playlists.db  ~/dotfiles/freetube/.config/FreeTube/playlists.db
-rsync profiles.db  ~/dotfiles/freetube/.config/FreeTube/profiles.db
-rsync search-history.db  ~/dotfiles/freetube/.config/FreeTube/search-history.db
-rsync settings.db  ~/dotfiles/freetube/.config/FreeTube/settings.db
+	sleep 6
+	cd  ~/.config/FreeTube/
+	rsync history.db  ~/dotfiles/freetube/.config/FreeTube/history.db
+	rsync playlists.db  ~/dotfiles/freetube/.config/FreeTube/playlists.db
+	rsync profiles.db  ~/dotfiles/freetube/.config/FreeTube/profiles.db
+	rsync search-history.db  ~/dotfiles/freetube/.config/FreeTube/search-history.db
+	rsync settings.db  ~/dotfiles/freetube/.config/FreeTube/settings.db
 
 
-cd ~/dotfiles
-a=$(git status |wc -l)
+	cd ~/dotfiles
+	a=$(git status |wc -l)
 
-if [ $a -gt 4 ]; then
-        wget -q --spider http://google.com > /dev/null
-        if [ $? -eq 0 ]; then
-        git add . && git commit -m "save" && git push -u origin main
-	notify-send " dotfiles synchronized "
-        fi
-else
-        exit 0
-fi
+	if [ $a -gt 4 ]; then
+        	wget -q --spider http://google.com > /dev/null
+	        if [ $? -eq 0 ]; then
+	        git add . && git commit -m "save" && git push -u origin main
+		notify-send " dotfiles synchronized "
+	        fi
+	else
+        	exit 0
+	fi
 done
 

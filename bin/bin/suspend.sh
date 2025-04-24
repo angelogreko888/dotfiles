7b16266f5638f19b62561898a8d96cd10c04096c
angelogreko <angelogreko@gmail.com>
2025-04-24 13:24:07 +0300

    save

---
+++ bin/bin/suspend.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+~/bin/cb.sh
+~/bin/gpush.sh
+systemctl suspend
--- nouse/bin/suspend.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-~/bin/gpush.sh
-sleep 1
-pidof hyprlock || (hyprlock & sleep 1s && systemctl suspend)

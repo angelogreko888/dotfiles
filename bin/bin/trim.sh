2e36ada854b0b7f34023c68053dc966a199070a6
angelogreko <angelogreko@gmail.com>
2025-04-26 08:56:31 +0300

    save

--- bin/bin/trim.sh
+++ bin/bin/trim.sh
@@ -7,17 +7,28 @@ handle_error() {
   trap 'handle_error $LINENO' ERR
 
 a=$(date | awk '{print $1}')
-if [ $a == Fri ];then
+if [ $a == Sat ];then
 	flag="/var/tmp/$(basename -- $0).flag"
 	if [ -e "$flag" ] ;then
-	[ $(date +%F) = $(date +%F -r "$flag") ] && exit 1
+	[ $(date +%F) = $(date +%F -r "$flag") ] && exit 0
 	fi
 	touch "$flag"
-                exec foot sh -c '
-		printf "\n\e[1;32m....... trim....... \n\n\e[0m"  
-                sudo fstrim -av;'
+	exec foot sh -c '
+	printf "\n\e[1;32m....... pacache ..... trim ....... \n\n\e[0m";
+	sudo paccache -ruk0
+	sudo paccache -rk1
+	sudo pacman -Scc --noconfirm
+	yay -Scc --noconfirm
+	yay -Yc --noconfirm
+	a="$((pacman -Qdtq) | wc -l >/dev/null)" > /dev/null
+	if [ $a > 0 ];then
+	        sudo pacman -Rnsc $(pacman -Qdtq)
+	else
+		echo
+	fi
+	sudo fstrim -av;'
 else
-		if [ -f /var/tmp/trim.sh.flag ]; then
-		rm /var/tmp/trim.sh.flag 
-		fi
+	if [ -f /var/tmp/trim.sh.flag ]; then
+	rm /var/tmp/trim.sh.flag 
+	fi
 fi
--- nouse/bin/trim.sh
+++ nouse/bin/trim.sh
@@ -7,17 +7,17 @@ handle_error() {
   trap 'handle_error $LINENO' ERR
 
 a=$(date | awk '{print $1}')
-
 if [ $a == Fri ];then
-        if ! [ -f ~/.log/trim.log ]; then
+	flag="/var/tmp/$(basename -- $0).flag"
+	if [ -e "$flag" ] ;then
+	[ $(date +%F) = $(date +%F -r "$flag") ] && exit 1
+	fi
+	touch "$flag"
                 exec foot sh -c '
 		printf "\n\e[1;32m....... trim....... \n\n\e[0m"  
-                sudo fstrim -av;
-                echo $(date) > ~/.log/trim.log;
-                '
-        fi
+                sudo fstrim -av;'
 else
-	if [ -f ~/.log/trim.log ]; then
-		rm ~/.log/trim.log 
-	fi
+		if [ -f /var/tmp/trim.sh.flag ]; then
+		rm /var/tmp/trim.sh.flag 
+		fi
 fi

e1eab1a54d58c23ec66cc56a6c5aac3bb9395e91
angelogreko <angelogreko@gmail.com>
2025-04-30 15:21:10 +0300

    save

--- 0inst/r.sh
+++ 0inst/r.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-gh="https://raw.githubusercontent.com/angelogreko888/dotfiles/main/00inst/"
+gh="https://raw.githubusercontent.com/angelogreko888/dotfiles/main/0inst/"
 ef="050grubefi.sh"
 gt="060grubgpt.sh"
+bt="030boot.sh"
 
 pacman -Sy --needed --noconfirm  wget nano 
 
 printf "\n\e[1;32m... Enter your choice ... : \n\e[0m"
 
-select  bm in efi gpt;do
+select  bm in efi boot gpt;do
 	case $bm in
 		efi)
                         wget $gh$ef 
@@ -17,6 +18,10 @@ select  bm in efi gpt;do
 		gpt)
                         wget $gh$gt 
                         bb=$gt
+			break ;;
+		boot)
+			wget $gh$bt
+			bb=$bt
 			break ;;
 		*)
 		printf "\n\e[1;31m... !!! ERROR Enter Your Choice !!! ... \e[0m" >&2
@@ -33,4 +38,4 @@ fi
 
 rm 0*
 
-printf "\n\n\e[1;32m ... Done Root Setup! Type exit, umount -a and reboot ...\n\n\e[0m"
+printf "\n\n\e[1;32m ... Done Root Setup! Type exit, umount -R /mnt and reboot ...\n\n\e[0m"

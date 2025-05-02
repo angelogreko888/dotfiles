3163943287516aa01fcceca3fd7da758ce7c6f8b
angelogreko <angelogreko@gmail.com>
2025-05-02 10:20:59 +0300

    save

---
+++ bin/bin/pyyu.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+handle_error() {
+      echo "An error occurred on line $1"
+      exit 1
+  }
+
+  trap 'handle_error $LINENO' ERR
+
+	sudo paccache -ruk0
+	sudo paccache -rk1
+	sudo pacman -Scc --noconfirm
+	yay -Scc --noconfirm
+	yay -Yc --noconfirm
+	a="$((pacman -Qdtq) | wc -l" > /dev/null
+	if [ $a > 0 ];then
+	        sudo pacman -Rnsc $(pacman -Qdtq)
+	else
+		echo
+	fi

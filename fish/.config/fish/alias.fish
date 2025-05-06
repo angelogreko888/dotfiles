e0c287cc66249a82360db480228f8ab20fe4c94e
angelogreko <angelogreko@gmail.com>
2025-05-06 23:13:13 +0300

    save

--- fish/.config/fish/alias.fish
+++ fish/.config/fish/alias.fish
@@ -153,4 +153,4 @@ alias lp='sudo -sE tlpui'
 alias fc="sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches'"
 alias mm='hyprctl keyword monitor HDMI-A-5,1920x1080@60.0,0x0,1'
 alias mmm='hyprctl keyword monitor HDMI-A-5,disable && hyprctl keyword monitor HDMI-A-5,preferred,auto'
-alias ee='ps ac | rg sleep'
+alias ee='pgrep sleep'

58e518bffc921c66e71b9c3e31de9d6387350f99
angelogreko <angelogreko@gmail.com>
2025-05-09 19:19:59 +0300

    save

--- bin/bin/sleep.sh
+++ bin/bin/sleep.sh
@@ -3,7 +3,7 @@ handle_error() {
       echo "An error occurred on line $1"
       exit 1
   }
-
   trap 'handle_error $LINENO' ERR
+
 
 pgrep  sleep &> /dev/null || rm /var/tmp/sync.sh.flag && ~/bin/sync.sh &

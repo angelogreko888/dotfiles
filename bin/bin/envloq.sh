06c4f735b5ec28ef1c83d6f1a5b7d3cb8f875cec
angelogreko <angelogreko@gmail.com>
2025-05-10 19:31:54 +0300

    save

---
+++ bin/bin/envloq.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+handle_error() {
+      echo "An error occurred on line $1"
+      exit 1
+  }
+
+  trap 'handle_error $LINENO' ERR
+
+a=$(hostnamectl | rg Model | awk '{print $3}')
+
+if [ $a == LOQ ];then
+	env = AQ_DRM_DEVICES,/dev/dri/card1:/dev/dri/card0
+else 
+	echo
+fi

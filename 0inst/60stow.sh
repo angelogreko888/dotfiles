14e996cf25698c531172c951579d177b1b93e1ca
angelogreko <angelogreko@gmail.com>
2025-05-12 10:38:45 +0300

    save

--- 0inst/60stow.sh
+++ 0inst/60stow.sh
@@ -24,9 +24,6 @@ gtk-3.0
 home
 hypr
 ranger
-#dunst
-swaync
-#uwsm
 waybar
 xsettingsd
 )
@@ -35,19 +32,28 @@ for ST in "${stow[@]}"; do
 	stow "$ST"
 done
 
-printf "\n\e[1;32m... Do You Want To Stow kanshi?  ... : \e[0m"
+plst=(
+swaync
+kanshi
+dunst
+uwsm
 
-select  yn in yes no;do
-        case $yn in
-                yes)
-                        stow kanshi
-                        break ;;
-                no)
-                        printf "\n\e[1;32m... OK ... : \n\e[0m"
-                        break ;;
-                *)
-                        printf "\n\e[1;31m... !!! ERROR Enter Your Choice !!! ... \e[0m" >&2
-        esac
+)
+
+for PLST in "${plst[@]}";do
+	printf "\n\e[1;32m... Do You Want To stow "$PLST"  ?  ... : \n\e[0m"
+	select  yn in yes no;do
+		case $yn in
+		yes)
+			stow "$PLST"
+			break ;;
+		no)
+			printf "\n\e[1;32m... OK ... : \n\e[0m"
+			break ;;
+		*)
+			printf "\n\e[1;31m... !!! ERROR Enter Your Choice !!! ... \e[0m" >&2
+		esac
+	done
 done
 
 cd ~

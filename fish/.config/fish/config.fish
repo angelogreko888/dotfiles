beafb6be55457c1aca79e3819044964033e9c54a
angelogreko <angelogreko@gmail.com>
2025-05-15 10:11:29 +0300

    save

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -21,7 +21,7 @@ function fish_prompt
         set stat (set_color red)"[$last_status]"(set_color normal)
     end
 
-    echo \n (set_color -o yellow)  ∋∈ ∋∈ ∋∈ ∋∈ ∋∈ (set_color -o green)   (set_color -oi blue) (prompt_pwd --full-length-dirs 8)    (set_color -o yellow) $stat '  ' 
+    echo \n (set_color -o brblue)  ∋∈ (set_color -o brwhite)∋∈ (set_color -o brblue)∋∈ (set_color -o brwhite)∋∈ (set_color -o brblue)∋∈ (set_color -o brwhite)   (set_color -oi blue) (prompt_pwd --full-length-dirs 8)    (set_color -o brblue) $stat '  ' 
 end
 
 export ANDROID_HOME=/opt/android-sdk/

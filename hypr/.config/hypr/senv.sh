508eda5ec6f2a55dec6221afb73bfbcaf62c6e64
angelogreko <angelogreko@gmail.com>
2025-06-18 13:16:04 +0300

    save

--- hypr/.config/hypr/senv.sh
+++
@@ -1,9 +0,0 @@
-#! usr/bin/env bash
-
-a=$(hostnamectl | rg Model | awk '{print $3}')
-
-if [ $a == LOQ ];then
-env = AQ_DRM_DEVICES,/dev/dri/card1:/dev/dri/card0
-elif [ $a == Lenovo ];then
-#env = AQ_DRM_DEVICES,/dev/dri/card1:/dev/dri/card0
-fi

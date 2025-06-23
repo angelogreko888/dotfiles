3b3785487a6bc25c920407e0e900724781857d09
angelogreko <angelogreko@gmail.com>
2025-06-23 15:04:13 +0300

    save

---
+++ bin/bin/phonetoshared.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+echo "start sync"
+
+cd /run/user/1000/gvfs/
+cd mtp:host=Xiaomi_Redmi_Note_13_dc536453
+cd 'Internal shared storage'
+
+rsync -a Pictures/ /mnt/shared-data/phone/Pictures
+
+rsync -a DCIM/ /mnt/shared-data/phone/DCIM
+
+rsync -a Documents/ /mnt/shared-data/phone/Documents
+
+rsync -a Download/ /mnt/shared-data/phone/Download
+
+rsync -a Movies/ /mnt/shared-data/phone/Movies
+
+rsync -a Music/ /mnt/shared-data/phone/Music
+
+rsync -a Recordings/ /mnt/shared-data/phone/Recordings
+
+echo "finished sync"

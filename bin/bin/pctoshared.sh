a6afdcc15b4498ff59f660b273f64c990f378a3b
angelogreko <angelogreko@gmail.com>
2025-07-08 15:03:37 +0300

    save

--- bin/bin/pctoshared.sh
+++ bin/bin/pctoshared.sh
@@ -3,13 +3,12 @@
 a=$(lsblk | grep shared | awk '{print $7}')
 
 if [ $a == /mnt/shared-data ] &> /dev/null;then
+echo "shared-data mounted"
+else 
+echo "mounting shared-data......"
 sudo mount /dev/sdb /mnt/shared-data/
+fi
+
 echo "start sync"
 rsync -a ~/vago/ /mnt/shared-data/vago/  --delete
 echo "sync finished"
-else 
-        echo "shared-data not mounted... quiting..."
-	exit 0
-fi
-
-
--- bin/bin/sharedtopc.sh
+++ bin/bin/sharedtopc.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 a=$(lsblk | grep shared | awk '{print $7}')
+
 if [ $a == /mnt/shared-data ] &> /dev/null;then
+echo "shared-data mounted"
+else 
+echo "mounting shared-data......"
+sudo mount /dev/sdb /mnt/shared-data/
+fi
+
 echo "start sync"
 rsync -a /mnt/shared-data/vago/ ~/vago/ --delete
 echo "sync finished"
-else 
-        echo "shared-data not mounted... quiting..."
-        exit 0
-fi
 

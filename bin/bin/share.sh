1d4a3aa2bdb3c4c6624095bef133afbc57d365fb
angelogreko <angelogreko@gmail.com>
2025-07-12 10:14:44 +0300

    save

---
+++ bin/bin/share.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+a=$(lsblk | grep shared | awk '{print $7}')
+
+if [ $a == /mnt/shared-data ] &> /dev/null;then
+echo "shared-data mounted"
+else 
+echo "mounting shared-data......"
+sudo mount /dev/sdb /mnt/shared-data/
+fi
+
+echo "start sync"
+rsync -a ~/vago/ /mnt/shared-data/vago/
+rsync -a /mnt/shared-data/vago/ ~/vago/
+echo "sync finished"
+echo "unmounting ...................."
+sudo umount /mnt/shared-data

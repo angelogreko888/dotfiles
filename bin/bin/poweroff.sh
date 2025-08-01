a3a6f8965c9712459b15fbb84682143dc6d9df8f
angelogreko <angelogreko@gmail.com>
2025-08-01 09:10:18 +0300

    save

--- bin/bin/poweroff.sh
+++ bin/bin/poweroff.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
 rm -rf ~/.cache/* 
-sleep 0.1
 ~/bin/cb.sh
-sleep 0.1
 ~/bin/bup.sh
-sleep 0.1
 ~/bin/gpush.sh
-sleep 0.1
 pkill sleep
 rm /var/tmp/sync.sh.flag
 rm /var/tmp/gitpull.sh.flag
